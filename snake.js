91efae76215ab613bd3bf5c2cd2c1da34d8d8fe2
abhasb <55402651+abhasb@users.noreply.github.com>
2019-10-12 08:54:42 -0600

    EventListner

--- snake.js
+++ snake.js
@@ -17,6 +17,22 @@ let beer = {
         x : Math.floor(Math.random()*17+1) * grid,
         y : Math.floor(Math.random()*15+1) * grid, 
     }
+
+let d;
+document.addEventListener("keydown",(e)=>){
+     let k  = e.keyCode;
+     if(k == 37 && d!= "right"){
+	d="left"
+     }else if(k == 38 && d!= "down"){
+	d="up"
+     }else if(k== 39 && d!= "left"){
+	d="right"
+     }else if(k==40 && d!= "up"){
+	d="down"
+     }
+     console.log(d);
+})
+
 function draw(){
     ctx.drawImage(g,0,0);
     
