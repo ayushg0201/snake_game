0e9e4aa40190f82428e549e3f682d3919a6910b9
rishabhsingh23 <54117489+rishabhsingh23@users.noreply.github.com>
2019-10-12 16:55:41 +0530

    Update snake.js

--- snake.js
+++ snake.js
@@ -1,5 +1,5 @@
 var canvas=document.getElementById("snake");
 var ctx=canvas.getContext("2d");
-ctx.fillStyle="red";
-ctx.fillRect(20,20,32,32);
+const grid = 32;
+var g = document.getElementById("img");
 
