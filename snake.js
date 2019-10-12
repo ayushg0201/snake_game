02af05c855decab5cd77c9c9edab18285cae7897
ayushg0201 <55704851+ayushg0201@users.noreply.github.com>
2019-10-12 20:00:35 +0530

    draw function

--- snake.js
+++ snake.js
@@ -17,3 +17,11 @@ let beer = {
         x : Math.floor(Math.random()*17+1) * grid,
         y : Math.floor(Math.random()*15+1) * grid, 
     }
+function draw(){
+    ctx.drawImage(g,0,0);
+    
+    for(let i = 0; i < snake.length ; i++){
+        ctx.fillRect(snake[i].x,snake[i].y,grid,grid);
+        ctx.strokeStyle = "white";
+        ctx.strokeRect(snake[i].x,snake[i].y,grid,grid);
+    }
